e53d3ee4ca5ffd4379097989b780df24fd1a7c53
Xavier <xlarue@macadamian.com>
2015-01-09 16:12:29 -0500

    ( ) comment now works.

--- lib/kernel.f
+++ lib/kernel.f
@@ -105,3 +105,25 @@
     ' NOT ,
     [COMPILE] IF
 ;
+
+: ( IMMEDIATE
+    1
+    BEGIN
+        KEY
+        DUP EMIT
+        DUP '(' =
+        IF
+              DROP
+              1+
+        ELSE
+            ')' =
+            IF
+                1 -
+            THEN
+        THEN
+    DUP 0= UNTIL
+    DROP
+;
+
+: NIP ( x y -- y ) SWAP DROP ;
+: TUCK ( x y -- y x y ) SWAP OVER ;
