2d00e97133c831b9edccc5cdfee9220fc4e54576
Xavier <xlarue@macadamian.com>
2015-01-12 09:50:25 -0500

    Working web version. Added buffered string to match jquery terminal

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -2,26 +2,39 @@ $(document).ready(function () {
     var JsForth = require('../../lib/jsForth.js');
     var kernelF = require('fs').readFileSync(__dirname + '/../../lib/kernel.f', 'utf8');
 
-    console.log("STARTING THE BEAST");
     var jsForth = null;
+
+    var bufferedStr = ""
+
+    var outputBufferedStr = function () {
+        t.echo(bufferedStr);
+        bufferedStr = "";
+    };
 
     var t = $('#term').terminal(function(command, term) {
         jsForth.pushIntoInputBuffer(command + '\n');
+        if (bufferedStr !== "") {
+            outputBufferedStr();
+        }
     }, {
-        greetings: 'jsForth',
+        greetings: 'starting jsForth...',
         name: 'jsForth',
         height: 500,
         prompt: 'forth> '
     });
 
     var cOut = function (c) {
-//        t.insert(String.fromCharCode(c));
-        t.echo(String.fromCharCode(c));
+        if (c === 13) {
+            outputBufferedStr();
+        } else {
+            bufferedStr += String.fromCharCode(c)
+        };
     };
     var lOut = function (arg) {
         t.echo(arg);
     };
 
     jsForth = new JsForth(lOut, cOut);
+    t.echo("Loading forth kernel...");
     jsForth.pushIntoInputBuffer(kernelF);
 });
