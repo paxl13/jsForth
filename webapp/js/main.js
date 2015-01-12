5c70ba89fd98f3732b6d39e5d7d1f919597a70cd
Xavier <xlarue@macadamian.com>
2015-01-11 20:16:04 -0500

    WORKING WEB VERSION.... somewhat :)

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,47 +1,26 @@
 var JsForth = require('../../lib/jsForth.js');
+
 $(document).ready(function () {
+    console.log("STARTING THE BEAST");
+    var jsForth = null;
+
     var t = $('#term').terminal(function(command, term) {
-        term.echo(command);
+        jsForth.pushIntoInputBuffer(command + '\n');
     }, {
         greetings: 'jsForth',
         name: 'jsForth',
         height: 500,
-        prompt: '',
-        keypress: function (ev, term) {
-            console.log(ev);
-            var e = jQuery.Event("forthKey");
-            t.trigger(e);
-//            var c = String.fromCharCode(ev.charCode);
-//            term.insert(c);
-            return false;
-        },
-        keydown: function (ev, term) {
-            if (ev.keyCode === 13) {
-                term.insert('\n');
-                return false;
-            }
-            return true;
-    }});
-
-    var cOut = function (c) {
-        t.insert(String.fromCharCode(c));
-    };
-    var lOut = function (arg) {
-        t.insert(arg);
-    };
-    var kIn = function () {
-        t.resume();
-        console.log('reading the key');
-        t.pause();
-        return ' ';
-    };
-
-    t.on("forthKey", function () {
-        console.log("event was there");
+        prompt: 'forth> '
     });
 
-//    t.pause();
+    var cOut = function (c) {
+//        t.insert(String.fromCharCode(c));
+        t.echo(String.fromCharCode(c));
+    };
+    var lOut = function (arg) {
+        t.echo(arg);
+    };
 
-    console.log("STARTING THE BEAST");
-//  var jsForth = new JsForth(lOut, cOut, kIn);
+    jsForth = new JsForth(lOut, cOut);
+
 });
