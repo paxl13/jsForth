4bc23d927b922d7d51c1e641d6f807ca42fedf5f
Xavier <xlarue@macadamian.com>
2015-01-14 11:15:51 -0500

    Further implementation of the forth core.

--- lib/kernel.f
+++ lib/kernel.f
@@ -108,6 +108,20 @@
     [COMPILE] IF
 ;
 
+: DO IMMEDIATE
+    [COMPILE] BEGIN
+    ' >R DUP , ,
+;
+
+: LOOP IMMEDIATE
+    ' R> DUP , ,
+    ' 1+ ,
+    ' 2DUP ,
+    ' = ,
+    [COMPILE] UNTIL
+    ' DROP DUP , ,
+;
+
 : ( IMMEDIATE
     1
     BEGIN
@@ -125,6 +139,31 @@
         THEN
     DUP 0= UNTIL
     DROP
+;
+
+: CASE IMMEDIATE
+        0               ( push 0 to mark the bottom of the stack )
+;
+
+: OF IMMEDIATE
+        ' OVER ,        ( compile OVER )
+        ' = ,           ( compile = )
+        [COMPILE] IF    ( compile IF )
+        ' DROP ,        ( compile DROP )
+;
+
+: ENDOF IMMEDIATE
+        [COMPILE] ELSE  ( ENDOF is the same as ELSE )
+;
+
+: ENDCASE IMMEDIATE
+        ' DROP ,        ( compile DROP )
+        ( keep compiling THEN until we get to our zero marker )
+        BEGIN
+                ?DUP
+        WHILE
+                [COMPILE] THEN
+        REPEAT
 ;
 
 : NIP ( x y -- y ) SWAP DROP ;
@@ -287,12 +326,97 @@
 
 : ? @ . ;
 
-: KERNELF_VERSION 4 ;
+: CONSTANT IMMEDIATE
+    WORD CREATE DOCOL!
+    ' LIT ,
+    ,
+    ' EXIT ,
+;
+
+: VALUE IMMEDIATE
+    [COMPILE] CONSTANT
+;
+
+: VARIABLE IMMEDIATE
+    WORD CREATE DOCOL!
+    ' LIT ,
+    HERE @ 2 + ,
+    ' EXIT ,
+    0 ,
+;
+
+: TO IMMEDIATE
+    WORD FIND >DFA 1+
+
+    STATE @ IF
+        ' LIT ,
+        ,
+        ' ! ,
+    ELSE
+        !
+    THEN
+;
+
+: +TO IMMEDIATE
+    WORD FIND >DFA 1+
+
+    STATE @ IF          ( a -- )
+        ' LIT ,
+        ,               ( a -- )
+        ' DUP ,         ( n a -- n a a )
+        ' -ROT ,        ( n a a -- a n a )
+        ' @ ,           ( a n a -- a n v )
+        ' + ,           ( a n v -- a n )
+        ' SWAP ,        ( a n -- n a )
+        ' ! ,
+    ELSE                ( n a -- )
+        DUP             ( n a -- n a a )
+        -ROT            ( n a a -- a n a )
+        @               ( a n a -- a n v )
+        +               ( a n v -- a n )
+        SWAP            ( a n -- n a )
+        !
+    THEN
+;
+
+: >NAME                   ( a -- a )
+    2 +
+;
+
+: ID.
+    >NAME @ TELL
+;
+
+: WORDS
+    LATEST @
+    BEGIN
+        DUP ID. SPACE
+        @ DUP 0=
+    UNTIL
+    CR
+;
+
+: CFA>
+    3 -
+;
+
+0 VALUE TEMP
+
+: I
+    R> R> R>  ( -- r l i )
+    DUP     ( r l i -- r l i i )
+    TO TEMP ( r l i i -- r l i )
+    >R >R >R
+    TEMP
+;
+
+: KERNELF_VERSION 5 ;
 
 : GREETING
     ." Welcome to jsForth" CR
     ." jsForth Core Ver #" CORE_VERSION . CR
     ." Forth Kernel Ver #" KERNELF_VERSION . CR
+    MEMORY_SIZE HERE @ - . ." free memory cells remaining" CR
 ;
 
 GREETING
